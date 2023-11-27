eac0ef6169c317abe7024fc8790770269f226711
Ajgorek04 <igorblendzinski12@gmail.com>
2023-11-27 19:14:35 +0100

    adding containers for notes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,33 @@ import { AddInput } from "./components/AddInput/AddInput";
 function App() {
     const [isAddNoteShown, setIsAddNoteShown] = useState(false);
 
+    const [notes, setNotes] = useState([
+        {
+            title: "Title 1",
+            content: "Cos tam notatki jakies lorem ipsum nie dziala tuta",
+        },
+        {
+            title: "Title 2",
+            content:
+                "Cfsaos tam no satatgas ki jakiefass lasf gasgorem asgas gipsumfas nie dzialffa tuta",
+        },
+        {
+            title: "Title 2",
+            content:
+                "Cfsaos tam no satatgas ki jakiefass lasf gasgorem asgas gipsumfas nie dzialffa tuta",
+        },
+        {
+            title: "Title 2",
+            content:
+                "Cfsaos tam no satatgas ki jakiefass lasf gasgorem asgas gipsumfas nie dzialffa tuta",
+        },
+        {
+            title: "Title 2",
+            content:
+                "Cfsaos tam no satatgas ki jakiefass lasf gasgorem asgas gipsumfas nie dzialffa tuta",
+        },
+    ]);
+
     function handleViewAddNoteClick() {
         setIsAddNoteShown(true);
     }
@@ -21,6 +48,15 @@ function App() {
                 showAddNote={setIsAddNoteShown}
             />
             {isAddNoteShown && <AddInput addNote={handleAddNoteClick} />}
+            <div className="noteContainer">
+                {notes.map((note, index) => (
+                    <div className="noteDivs" key={index}>
+                        <h1>{note.title}</h1>
+                        <hr />
+                        <p>{note.content}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
